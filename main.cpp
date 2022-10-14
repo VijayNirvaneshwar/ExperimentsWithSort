2431981c9271103ff6a21b3ca122d6282b9af213
VijayNirvaneshwar <vnir769@aucklanduni.ac.nz>
2022-10-14 19:33:53 +1300

    Program Done (MergeSort might be incorrect

--- main.cpp
+++ main.cpp
@@ -1,30 +1,118 @@
 #include <iostream>
+#include <cstdlib>
+#include <chrono>
 
-#define NUM_INT 11
+#define NUM_INT 100000
 #define MAX_INT 100
 
 using namespace std;
+using namespace chrono;
 
 void selectionSort(int *, int); // forward declaration of function in sorts.cpp
+void insertionSort(int *, int);
+void bubbleSort(int *array, int n);
+void mergeSort(int *array, int firstIndex, int lastIndex);
 
 int main() {
 
+  srand(time(0));
+
+  time_point<high_resolution_clock> start, end;
+  duration<double> elapsedSeconds;
+
   int array[NUM_INT];
 
-  for (int ii=0; ii<NUM_INT; ii++) array[ii] = NUM_INT - ii;
+  /*  SELECTION SORT  */
 
-  cout << "input data: ";
-  for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  cout << "Selection sort!" << endl;
+
+  for (int ii=0; ii<NUM_INT; ii++) array[ii] = rand() / MAX_INT;
+
+  // cout << "input data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+
+  start = high_resolution_clock::now();
+
+  selectionSort(array, NUM_INT);
+
+  end = high_resolution_clock::now();
+  elapsedSeconds = (end - start) * 1000;
+
+  // cout << "sorted data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+  cout << "elapsed time: " << elapsedSeconds.count() << "ms" << endl;
   cout << endl;
 
-  selectionSort(array, NUM_INT); cout << "Selection sort!" << endl;
+  /*  INSERTION SORT  */
 
-  cout << "sorted data: ";
-  for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  cout << "Insertion sort!" << endl;
+
+  for (int ii=0; ii<NUM_INT; ii++) array[ii] = rand() / MAX_INT;
+
+  // cout << "input data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+
+  start = high_resolution_clock::now();
+
+  insertionSort(array, NUM_INT);
+
+  end = high_resolution_clock::now();
+  elapsedSeconds = (end - start) * 1000;
+
+  // cout << "sorted data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+  cout << "elapsed time: " << elapsedSeconds.count() << "ms" << endl;
+  cout << endl;
+
+  /*  BUBBLE SORT  */
+
+  cout << "Bubble sort!" << endl;
+
+  for (int ii=0; ii<NUM_INT; ii++) array[ii] = rand() / MAX_INT;
+
+  // cout << "input data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+
+  start = high_resolution_clock::now();
+
+  bubbleSort(array, NUM_INT);
+
+  end = high_resolution_clock::now();
+  elapsedSeconds = (end - start) * 1000;
+
+  // cout << "sorted data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+  cout << "elapsed time: " << elapsedSeconds.count() << "ms" << endl;
+  cout << endl;
+
+  /* MERGE SORT */
+  cout << "Merge sort!" << endl;
+
+  for (int ii=0; ii<NUM_INT; ii++) array[ii] = rand() / MAX_INT;
+
+  // cout << "input data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+
+  start = high_resolution_clock::now();
+
+  mergeSort(array, 0, (NUM_INT-1)); 
+
+  end = high_resolution_clock::now();
+  elapsedSeconds = (end - start) * 1000;
+
+  // cout << "sorted data: ";
+  // for (int ii=0; ii<NUM_INT; ii++) cout << array[ii] << " ";
+  // cout << endl;
+  cout << "elapsed time: " << elapsedSeconds.count() << "ms" << endl;
   cout << endl;
 
   return 0;
-
-  // Hi this is a test
 }
 
--- sorts.cpp
+++ sorts.cpp
@@ -16,3 +16,52 @@ void selectionSort(int *array, int n) {
   } 
 }
 
+// Author: Luke Hallum (modified by Vijay Nirvaneshwar)
+void insertionSort (int *array, int n) {
+  for (int i = 1; i < n; i++) {
+    for (int j = i - 1; j >= 0 && (array[j] > array[j+1]); j--) {
+      swapTwo(array, j, j + 1);
+    }
+  }
+}
+
+// Author: Vijay Nirvaneshwar
+void bubbleSort(int *array, int n) {
+  for (int i = 1; i < n;  i++) {
+    for (int j = 1; j < n - i + 1; j++) {
+      if (array[j] < array[j-1]) {
+        swapTwo(array, j, j-1);
+      }
+    }
+  }
+}
+
+// Author: Vijay Nirvaneshwar
+void mergeSwap(int *array, int firstIndex, int lastIndex) {
+  // Find middle index
+  int middleIndex = (firstIndex + lastIndex) / 2;
+
+  for (int i = 0; i <= middleIndex; i++) {
+    for (int j = (middleIndex + 1); j <= lastIndex; j++) {
+      if (array[i] > array[j]) {
+        swapTwo(array, i, j);
+        break;
+      }
+    }
+  }
+}
+
+// Author: Vijay Nirvaneshwar
+void mergeSort(int *array, int firstIndex, int lastIndex) {
+  if (firstIndex < lastIndex) {
+    int middleIndex = (firstIndex + lastIndex) / 2;
+
+    // Recursively call mergesort for first half and second half
+    mergeSort(array, firstIndex, middleIndex);
+    mergeSort(array, (middleIndex + 1), lastIndex);
+
+    // Merge arrays
+    mergeSwap(array, firstIndex, lastIndex);
+  }
+}
+
